4c076806ddf92eb35828a82a5119d4bcda1df54d
Houang869 <48252530+Houangnt@users.noreply.github.com>
2024-10-18 11:50:04 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using YoloDotNet;
+using YoloDotNet.Enums;
+using YoloDotNet.Models;
+using YoloDotNet.Extensions;
+using SkiaSharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Instantiate a new Yolo object
+        using var yolo = new Yolo(new YoloOptions
+        {
+            OnnxModel = @"yolo11x.onnx",  
+            ModelType = ModelType.ObjectDetection,
+            Cuda = false  
+        });
+
+        using var image = SKImage.FromEncodedData(@"test.jpg");
+
+        var results = yolo.RunObjectDetection(image, confidence: 0.25, iou: 0.7);
+
+        using var resultImage = image.Draw(results);
+
+        resultImage.Save(@"result.jpg", SKEncodedImageFormat.Jpeg, 80);
+    }
+}
