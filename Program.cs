5336f59d9ea6815e7d0242648c0660ff5a0e452e
Houang869 <48252530+Houangnt@users.noreply.github.com>
2024-10-18 15:38:02 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,30 @@
-﻿using YoloDotNet;
+using YoloDotNet;
 using YoloDotNet.Enums;
 using YoloDotNet.Models;
 using YoloDotNet.Extensions;
 using SkiaSharp;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Instantiate a new Yolo object
         using var yolo = new Yolo(new YoloOptions
         {
-            OnnxModel = @"yolo11x.onnx",  
+            OnnxModel = @"yolo11x.onnx",
             ModelType = ModelType.ObjectDetection,
-            Cuda = false  
+            Cuda = false
         });
 
         using var image = SKImage.FromEncodedData(@"test.jpg");
+        List<ObjectDetection> results = yolo.RunObjectDetection(image, confidence: 0.45, iou: 0.5);
 
-        var results = yolo.RunObjectDetection(image, confidence: 0.25, iou: 0.7);
+        foreach (var result in results)
+        {
+            Console.WriteLine($"Label: {result.Label}, Confidence: {result.Confidence}, X: {result.BoundingBox.Left}, Y: {result.BoundingBox.Top}, Width: {result.BoundingBox.Width}, Height: {result.BoundingBox.Height}");
+        }
 
         using var resultImage = image.Draw(results);
-
         resultImage.Save(@"result.jpg", SKEncodedImageFormat.Jpeg, 80);
     }
 }
